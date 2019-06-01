ba712ef7f837dd79e294784f78601231edc1a117
kavya654 <kavya.narayan654@gmail.com>
2019-06-01 11:39:40 +0530

    done
    done

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -45,6 +45,9 @@ public class CartController extends HttpServlet {
 				case "/addcart":
 					 addToCart(request, response);
            break;
+				case "/delete":
+					 deleteFromCart(request, response);
+          break;
         default:
            break;
 			}
@@ -56,7 +59,15 @@ public class CartController extends HttpServlet {
 		response.sendRedirect("../ShoppingCart.jsp");
 	}
 
-  protected void addToCart(HttpServletRequest request, HttpServletResponse response)
+  private void deleteFromCart(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException {
+	  
+	  HttpSession session = request.getSession();
+	  int index = Integer.parseInt(request.getParameter("index"));
+	  ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+	  cart.deleteCartItem(index);
+	}
+
+protected void addToCart(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
    HttpSession session = request.getSession();
    String idStr = request.getParameter("id");
